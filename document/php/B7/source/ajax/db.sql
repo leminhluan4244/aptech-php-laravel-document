6657dfbcc7fab2620194b68e93a21f0a1a35a447
Minh Luan Le <leminhluan4244@gmail.com>
2024-08-06 20:26:07 +0700

    Update source

--- document/php/B7/source/ajax/db.sql
+++ document/php/B7/source/ajax/db.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE test01;
+
 USE test01;
 
 CREATE TABLE customer (
@@ -7,10 +8,29 @@ CREATE TABLE customer (
     bill int(10) NOT NULL,
     year int(10) NOT NULL,
     PRIMARY KEY (name)
-)
+);
 
-INSERT INTO customer (name, gender, bill, `year`) VALUES ('Paul', 'm', 10000, 1998);
-INSERT INTO customer (name, gender, bill, `year`) VALUES ('Tyron', 'm', 5000, 1992);
-INSERT INTO customer (name, gender, bill, `year`) VALUES ('Hans', 'm', 25000, 1994);
-INSERT INTO customer (name, gender, bill, `year`) VALUES ('Erin', 'f', 12000, 1997);
-INSERT INTO customer (name, gender, bill, `year`) VALUES ('Vinn', 'f', 2000, 2000);
+INSERT INTO
+    customer (name, gender, bill, `year`)
+VALUES
+    ('Paul', 'm', 10000, 1998);
+
+INSERT INTO
+    customer (name, gender, bill, `year`)
+VALUES
+    ('Tyron', 'm', 5000, 1992);
+
+INSERT INTO
+    customer (name, gender, bill, `year`)
+VALUES
+    ('Hans', 'm', 25000, 1994);
+
+INSERT INTO
+    customer (name, gender, bill, `year`)
+VALUES
+    ('Erin', 'f', 12000, 1997);
+
+INSERT INTO
+    customer (name, gender, bill, `year`)
+VALUES
+    ('Vinn', 'f', 2000, 2000);
